ec273b1300b0fbafd744cc21b6b9da2a5d2cb535
nethical <aguptaq88@gmail.com>
2022-10-08 20:21:50 +0530

    Added a cool splash text

--- bot.py
+++ bot.py
@@ -1,6 +1,20 @@
 from utils.shell import shell
 from utils.files import files
-hello = "hi"
+
+print('''
+    
+         _____   _      _           
+        |_   _|_| |_ __(_)_   _____ 
+          | |/ _` | '__| \ \ / / _ |
+          | | (_| | |  | |\ V /  __/
+          |_|\__,_|_|  |_| \_/ \___|
+        
+        
+
+        Welcome to TDrive.
+        Created By Nethical \n\n
+        ''')
+
 files.generateFileSystem()
 f = open("./data/filesystem.json", "r")
 shell(f.read())
--- utils/telegram.py
+++ utils/telegram.py
@@ -13,43 +13,4 @@ class telegram:
         creds = telegram.getCreds()
         return pyrogram.Client("my_account", creds["id"], creds["hash"])
 
-    def saveAllMessages():
-        app = telegram.getApp()
-        creds = telegram.getCreds()
-        with app:
-            f = open("./cache/msgs", "w")
-            y = []
-            for message in app.get_chat_history(creds["username"]):
-                x = {"directory": message.text,
-                    "msg_id": message.id,
-                    "chat_id" : message.chat.id,
-                    "type":"file",
-                    "doocument" : files.toJSON(str(message.document))}
-                y.append(x)
-            f.write(json.dumps(y))
-            f.close()
-
-    def listAllFilesFrom(chatId,msgId) -> list:
-        app = telegram.getApp()
-        # creds = telegram.getCreds()
-        # f = open("./cache/msgs","r")
-        # y = json.loads(f.read())
-        with app:
-            for msg in app.get_messages("TdriveDatabot",reply_to_message_ids=54423):
-                print(msg)
-        return []
-
-    def generateFileSystem(f) -> dict:
-        f = open("./cache/msgs","r")
-        y = json.loads(f.read())
-        listOfFiles = {}
-
-        for msg in y:
-            x = str(msg["directory"]).split("/")
-            
-            print(reduce(lambda x,y: {y:x},reversed(x)))
-
-
-            # listOfFiles.append()
-        # print(listOfFiles)
-        return listOfFiles
+    
